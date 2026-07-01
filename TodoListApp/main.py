41dd67274efcfea8b79a340cbc6b0aa4285456f0
Nate <nateswanson213@gmaill.com>
2026-07-01 17:20:59 -0500

    Added complete as a feature

--- TodoListApp/main.py
+++ TodoListApp/main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type 'add', 'show', 'edit' or 'exit': ")
+    user_action = input("Type 'add', 'show', 'edit', 'complete' or 'exit': ")
     user_action = user_action.strip()
 
     match user_action:
@@ -9,13 +9,16 @@ while True:
             todo = input("Enter a task to add: ")
             todos.append(todo)
         case 'show':
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos):
+                print(f"{index + 1}. {item}")
         case 'edit':
             number = int(input("Which task would you like to edit? (Enter the number.): ")) 
             number = number - 1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
+        case 'complete':
+            number = int(input("Number of the todo that is complete: "))
+            todos.pop(number - 1)
         case 'exit':
             break
 
