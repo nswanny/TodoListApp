b4fddacfe6dc9b43ed4591f8f0fc21adc332d079
Nate <nateswanson213@gmaill.com>
2026-07-02 01:48:33 -0500

    Optimized code, fixed 'edit' and 'complete' commands.

--- TodoListApp/main.py
+++ TodoListApp/main.py
@@ -40,11 +40,14 @@ while True:
 
             with open('todos.txt', 'r') as file:
                 todos = file.readlines()
-
-            todos.pop(number - 1)
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
 
             with open('todos.txt', 'w') as file:
                 file.writelines(todos)
+            
+            print(f"{todo_to_remove} was removed from the list")
 
         case 'exit':
             break
