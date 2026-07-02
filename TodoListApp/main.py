81f7f41a46aefdb260fe11b08ebb084f9a386ba3
Nate <nateswanson213@gmaill.com>
2026-07-02 01:29:08 -0500

    Removed the list comprehension as it was unnecessary

--- TodoListApp/main.py
+++ TodoListApp/main.py
@@ -6,39 +6,46 @@ while True:
         case 'add':
             todo = input("Enter a task to add: ") + "\n"
 
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            with  open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
             todos.append(todo)
 
-            file = open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
         case 'show':
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
             for index, item in enumerate(todos):
                 item = item.strip('\n')
                 print(f"{index + 1}. {item}")
-        case 'edit':
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
 
+        case 'edit':
             number = int(input("Which task would you like to edit? (Enter the number.): ")) 
             number = number - 1
-            new_todo = input("Enter new todo: ")
 
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            new_todo = input("Enter new todo: ")
+            
             todos[number] = new_todo + '\n'
-            file = open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
         case 'complete':
             number = int(input("Number of the todo that is complete: "))
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
             todos.pop(number - 1)
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
         case 'exit':
             break
 
