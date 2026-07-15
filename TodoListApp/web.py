92fdf159d858f36091438b006326a88b82a0b89c
Nate <nateswanson213@gmaill.com>
2026-07-15 14:16:32 -0500

    Implemented checkbox deleting task from list. Web app is finished.

--- TodoListApp/web.py
+++ TodoListApp/web.py
@@ -10,15 +10,17 @@ def add_todo():
 
 todos = functions.get_todos()
 
-st.title("My Todo App")
-st.subheader("This is my todo app.")
+st.title("Todo App")
 st.write("This app is to increase productivity.")
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Add new todo...", 
               on_change=add_todo, key='new_todo')
-
-st.session_state
